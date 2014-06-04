4dee432ebc5341a076238d33c30633c9b9d3e661
Luca Arcara <larcara@gmail.com>
2014-06-04 14:59:08 +0200

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,27 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require sb-admin/jquery.metisMenu
+//= require sb-admin/jquery.dataTables
+//= require sb-admin/dataTables.bootstrap
+//= require sb-admin/jquery.flot
+//= require sb-admin/jquery.flot.pie
+//= require sb-admin/jquery.flot.resize
+//= require sb-admin/jquery.flot.tooltip.min
+//= require sb-admin/raphael-2.1.0.min
+//= require sb-admin/morris
+
+//= require sb-admin/sb-admin
+
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+  def blank
+
+  end
+  def index
+
+  end
+  def login
+    render layout: "login"
+  end
+  def flot
+
+  end
+  def morris
+
+  end
+  def tables
+
+  end
+  def panels_wells
+
+  end
+  def buttons
+
+  end
+  def typography
+
+  end
+  def grid
+
+  end
+  def forms
+
+  end
+end
